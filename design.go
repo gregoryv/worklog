82c1f0cc150fdcb0de4d4f855aca47cd3e3fc925
Gregory Vincic <g@7de.se>
2018-08-08 06:55:43 +0200

    comment on design

--- design.go
+++ design.go
@@ -8,15 +8,15 @@ type lexer struct {
 	report chan part
 }
 
+// state funcs are used to scan for parts from specific context
 type state func(*lexer) state
 
+// tag enumerates recognized parts which the lexer may find
 type tag int
 
 type part struct {
-	tag  tag
-	pos  int
-	val  string
-	line int
+	tag tag
+	val string
 }
 
 const (
