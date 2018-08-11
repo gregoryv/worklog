a9be466c201fa5a57b7ac823fc876fdf26114912
Gregory Vincic <g@7de.se>
2018-08-11 21:36:12 +0200

    Add lexYear()

--- run.go
+++ run.go
@@ -1,8 +1,18 @@
 package timesheet
 
-func (l *Lexer) run(s *Scanner, out chan Part) {
+type lexFn func(s *Scanner, out chan Part) lexFn
+
+func lexYear(s *Scanner, out chan Part) lexFn {
 	pos := s.Pos()
 	val := s.ScanAll("0123456789")
 	out <- Part{tok: Number, val: val, pos: pos}
-	// todo define the grammar now...
+	s.Scan(" ")
+	return nil
+}
+
+func (l *Lexer) run(s *Scanner, out chan Part) {
+	// We expect to start the file with a year
+	for fn := lexYear; fn != nil; fn = fn(s, out) {
+	}
+	close(out)
 }
--- run_test.go
+++ run_test.go
@@ -4,6 +4,19 @@ import (
 	"testing"
 )
 
-func TestLexer_run(t *testing.T) {
-	todo(t)
+func Test_lexYear(t *testing.T) {
+	cases := []struct {
+		s   *Scanner
+		out chan Part
+	}{
+		{NewScanner("2018"), make(chan Part)},
+	}
+	for _, c := range cases {
+		go lexYear(c.s, c.out)
+		part := <-c.out
+		assert(t, "",
+			equals("", "Number[1,1]: \"2018\"", part.String()),
+		)
+		close(c.out)
+	}
 }
