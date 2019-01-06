3653049c6595cc6c5c9701951e163fa9750ea40c
Gregory Vincic <g@7de.se>
2019-01-06 11:43:51 +0100

    Print summary in worklog

--- cmd/worklog/main.go
+++ cmd/worklog/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	timesheet "github.com/gregoryv/go-timesheet"
 )
@@ -20,19 +21,27 @@ func main() {
 
 	p := timesheet.NewParser()
 	for _, path := range filePaths {
+		fmt.Print(path, " ")
 		body, err := ioutil.ReadFile(path)
 		fatal(err)
 
 		reported, err := p.SumReported(body)
 		fatal(err)
 		tag := timesheet.Tagged{reported, "reported"}
-		fmt.Println(tag.String())
+		fmt.Print(tag, " ")
+
 		tagged, err := p.SumTagged(body)
 		fatal(err)
 		for _, tag := range tagged {
-			fmt.Println(tag.String())
+			fmt.Print("(", tag, ") ")
 		}
+		fmt.Println()
 	}
+}
+
+func srender(tag timesheet.Tagged) string {
+	parts := strings.Split(tag.String(), " ")
+	return fmt.Sprintf("%7s %s", parts[0], parts[1])
 }
 
 func fatal(err error) {
